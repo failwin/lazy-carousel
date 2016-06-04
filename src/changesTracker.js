d7265b2d93be46116d67cc6070268a44b0a88291
y.kovalchuk <failwin@ukr.net>
2016-06-05 02:57:30 +0300

    ChangesTracker (remove comments element)

--- src/changesTracker.js
+++ src/changesTracker.js
@@ -3,7 +3,6 @@
 
 // Module
 var NG_REMOVED = '$$MY_NG_REMOVED';
-var ELEMENT_NODE = 1;
 
 function isArray(obj) {
     if (Array.isArray) {
@@ -14,10 +13,6 @@ function isArray(obj) {
 function isObject(obj) {
     var type = typeof obj;
     return type === 'function' || type === 'object' && !!obj;
-}
-function isString(obj) {
-    var type = typeof obj;
-    return type === 'string' && !!obj;
 }
 function isFunction(obj) {
     return Object.prototype.toString.call(obj) === '[object ' + name + ']';
@@ -69,19 +64,6 @@ function extend(obj, prop, isDeep) {
     return target;
 }
 
-function createMap() {
-    return Object.create(null);
-}
-
-function createComment(directiveName, comment, debug) {
-    var content = '';
-    if (debug) {
-        content = ' ' + (directiveName || '') + ': ';
-        if (comment) content += comment + ' ';
-    }
-    return window.document.createComment(content);
-}
-
 function createElement(str) {
     if (isObject(str)) {
         return str;
@@ -96,43 +78,9 @@ function createElement(str) {
     }
     return frag.childNodes[0];
 }
-function after(element, newElement) {
-    var index = element, parent = element.parentNode;
-
-    for (var i = 0, ii = newElement.length; i < ii; i++) {
-        var node = newElement[i];
-        parent.insertBefore(node, index.nextSibling);
-        index = node;
-    }
-}
-function prepend(element, node) {
-    if (element.nodeType === ELEMENT_NODE) {
-        var index = element.firstChild;
-        forEach(node, function(child) {
-            element.insertBefore(child, index);
-        });
-    }
-}
 
 function trackByIdFn(key, value, index, trackById) {
     return value[trackById];
-}
-
-function getBlockStart(block) {
-    return block.clone[0];
-}
-
-function getBlockEnd(block) {
-    return block.clone[block.clone.length - 1];
-}
-
-function extractElementNode(element) {
-    for (var i = 0; i < element.length; i++) {
-        var elm = element[i];
-        if (elm && elm.nodeType === ELEMENT_NODE) {
-            return elm;
-        }
-    }
 }
 
 function domInsert(element, parentElement, afterElement) {
@@ -143,8 +91,7 @@ function domInsert(element, parentElement, afterElement) {
     var parent = parentElement || afterElement.parentNode;
 
     if (afterElement) {
-        var afterNode = extractElementNode(afterElement);
-        if (afterNode && !afterNode.parentNode && !afterNode.previousElementSibling) {
+        if (afterElement && !afterElement.parentNode && !afterElement.previousElementSibling) {
             afterElement = null;
         }
     }
@@ -152,10 +99,15 @@ function domInsert(element, parentElement, afterElement) {
     element = createElement(element);
 
     if (afterElement) {
-        after(afterElement, element);
+        afterElement.parentNode.insertBefore(element, afterElement.nextSibling);
+
+        //after(afterElement, element);
     }
     else {
-        prepend(parent, element);
+
+        parent.insertBefore(element, parent.firstChild);
+
+        //prepend(parent, element);
     }
 
     //afterElement ? afterElement.after(element) : parentElement.prepend(element);
@@ -169,7 +121,6 @@ var ChangesTracker = (function() {
         this.$element = element;
 
         this.$startComment = null;
-        this.$endComment = null;
 
         this.lastBlockMap = null;
 
@@ -179,56 +130,48 @@ var ChangesTracker = (function() {
     ChangesTracker.prototype.defOpts = {
         debug: true,
         trackById: 'id',
-        beforeAdd: function(block, callback) {
+        beforeAdd: function(data, callback) {
             callback = callback || function() {};
 
-            //var elem = document.createElement('li');
-            //elem.innerHTML = block.scope.id;
-            var elem = createElement('<li>'+ block.scope.id +'</li>');
+            var elem = createElement('<li>'+ data.id +'</li>');
 
             callback(elem);
         },
-        afterAdd: function(block, element){},
-        beforeRemove: function(block, element, callback) {
+        afterAdd: function(data, element){},
+        beforeRemove: function(data, element, callback) {
             callback = callback || function () {};
 
             callback();
         },
-        afterRemove: function(block) {}
+        afterRemove: function(data) {}
     };
 
     ChangesTracker.prototype.init = function() {
-        this.$startComment = createComment('start ngRepeat', null, this.opts.debug);
-        this.$endComment = createComment('end ngRepeat', null, this.opts.debug);
-
         // clear
         while (this.$element.firstChild) {
             this.$element.removeChild(this.$element.firstChild);
         }
 
+        // insert first anchor
+        this.$startComment = window.document.createComment('');
         this.$element.appendChild(this.$startComment);
 
-        this.lastBlockMap = createMap();
+        this.lastBlockMap = Object.create(null);
     };
 
     ChangesTracker.prototype.updateList = function(collection) {
-        var index, length,
-            previousNode =  this.$startComment,
+        var previousNode =  this.$startComment,
             nextNode,
-            nextBlockMap = createMap(),
-            collectionLength,
-            key, value,
-            trackById,
-            collectionKeys,
-            block,
+            nextBlockMap = Object.create(null),
             nextBlockOrder,
-            elementsToRemove;
+            collectionLength,
+            index, key, value,
+            trackById,
+            block,
+            removed = [];
 
-        collectionKeys = collection;
-        collectionLength = collectionKeys.length;
+        collectionLength = collection.length;
         nextBlockOrder = new Array(collectionLength);
-
-        var removes = [];
 
         // locate existing items
         for (index = 0; index < collectionLength; index++) {
@@ -244,14 +187,14 @@ var ChangesTracker = (function() {
             } else if (nextBlockMap[trackById]) {
                 // if collision detected. restore lastBlockMap and throw an error
                 forEach(nextBlockOrder, function(block) {
-                    if (block && block.scope) {
+                    if (block && block.data) {
                         this.lastBlockMap[block.id] = block;
                     }
-                }, bind(this));
+                }.bind(this));
                 throw new Error('Duplicates in a repeater are not allowed');
             } else {
                 // new never before seen block
-                nextBlockOrder[index] = {id: trackById, scope: undefined, clone: undefined};
+                nextBlockOrder[index] = {id: trackById, data: undefined, element: undefined};
                 nextBlockMap[trackById] = true;
             }
         }
@@ -259,31 +202,25 @@ var ChangesTracker = (function() {
         // remove leftover items
         for (var blockKey in this.lastBlockMap) {
             block = this.lastBlockMap[blockKey];
-            elementsToRemove = block.clone;
 
             //$animate.leave(elementsToRemove);
-            removes.push(block);
+            removed.push(block);
 
-            if (elementsToRemove[0].parentNode) {
+            if (block.element.parentNode) {
                 // if the element was not removed yet because of pending animation, mark it as deleted
                 // so that we can ignore it later
-                for (index = 0, length = elementsToRemove.length; index < length; index++) {
-                    elementsToRemove[index][NG_REMOVED] = true;
-                }
+                block.element[NG_REMOVED] = true;
             }
-            //block.scope.$destroy();
         }
 
-        // we are not using forEach for perf reasons (trying to avoid #call)
+        // moving/inserting
         for (index = 0; index < collectionLength; index++) {
             key = index;
             value = collection[key];
             block = nextBlockOrder[index];
 
-            if (block.scope) {
-                // if we have already seen this object, then we need to reuse the
-                // associated scope/element
-
+            if (block.data) {
+                // if we have already seen this object
                 nextNode = previousNode;
 
                 // skip nodes that are already pending removal via leave animation
@@ -291,54 +228,42 @@ var ChangesTracker = (function() {
                     nextNode = nextNode.nextSibling;
                 } while (nextNode && nextNode[NG_REMOVED]);
 
-                if (getBlockStart(block) != nextNode) {
+                if (block.element != nextNode) {
                     // existing item which got moved
 
                     // $animate.move(getBlockNodes(block.clone), null, previousNode);
-                    // move: function(element, parent, after, options) {
-                    domInsert(block.clone, null, previousNode);
+                    domInsert(block.element, null, previousNode);
                 }
-                previousNode = getBlockEnd(block);
+                previousNode = block.element;
             } else {
                 // new item which we don't know about
-                block.scope = value;
+                block.data = value;
 
-
-                this.opts.beforeAdd(block, function(elem) {
-                    var clone = [];
-                    clone[0] = elem;
-                    // http://jsperf.com/clone-vs-createcomment
-                    var endNode = this.$endComment.cloneNode(false);
-                    clone[1] = endNode;
-
+                this.opts.beforeAdd(block.data, function(elem) {
                     // $animate.enter(clone, null, previousNode);
-                    // enter: function(element, parent, after, options) {
-                    domInsert(clone, null, previousNode);
+                    domInsert(elem, null, previousNode);
 
-                    this.opts.afterAdd(block, clone[0]);
+                    this.opts.afterAdd(block.data, elem);
 
-                    previousNode = endNode;
-                    // Note: We only need the first/last node of the cloned nodes.
-                    // However, we need to keep the reference to the jqlite wrapper as it might be changed later
-                    // by a directive with templateUrl when its template arrives.
-                    block.clone = clone;
+                    previousNode = elem;
+
+                    block.element = elem;
                     nextBlockMap[block.id] = block;
                 }.bind(this));
             }
         }
         this.lastBlockMap = nextBlockMap;
 
-        // MY real removing
-        for (var i = 0, l = removes.length; i < l; i++) {
-            var block = removes[i];
-            this.opts.beforeRemove(block, block.clone[0], function(){
-                this.$element.removeChild(block.clone[0]);
-                this.$element.removeChild(block.clone[1]);
-                this.opts.afterRemove(block);
-                block = block.scope = block.clone = block.clone[0] = block.clone[1] = null;
+        // real removing
+        for (var i = 0, l = removed.length; i < l; i++) {
+            var block = removed[i];
+            this.opts.beforeRemove(block.data, block.element, function(){
+                this.$element.removeChild(block.element);
+                this.opts.afterRemove(block.data);
+                block = block.data = block.element = null;
             }.bind(this));
         }
-        removes = null;
+        removed = null;
     };
 
     return ChangesTracker;
